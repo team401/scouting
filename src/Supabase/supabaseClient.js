2487317c842622c2ddd18fefac755aa9f0b8ca1f
GideonC <100504528+TheLumpiest@users.noreply.github.com>
2024-10-23 20:12:43 -0400

    Update supabaseClient.js

--- src/Supabase/supabaseClient.js
+++ src/Supabase/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_ANON_KEY;
-const supabase = createClient("https://ozkykrmuikqgrjfwemus.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96a3lrcm11aWtxZ3JqZndlbXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1NTgzOTQsImV4cCI6MjAyNDEzNDM5NH0.uw5P0WARTEleBzhNtraLsCEO2wp781rSzggxDI9dm8s");
+const supabase = createClient("https://hsmkwrrptltywjqljrpf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzbWt3cnJwdGx0eXdqcWxqcnBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3Mjg0OTIsImV4cCI6MjA0NTMwNDQ5Mn0.uyG-NkGTE--L4aFt96vEy5a6UuwntYxWLouQrG9tLGM");
 
 export default supabase;
