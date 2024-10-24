397612ec9630b27be9273c28b601a084712864ac
GideonC <100504528+TheLumpiest@users.noreply.github.com>
2024-10-23 20:49:51 -0400

    Update supabaseClient.js (#64)
    Co-authored-by: Bread-n-Butter0 <ep123forest@icloud.com>

--- src/Supabase/supabaseClient.js
+++ src/Supabase/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_ANON_KEY;
+
 const supabase = createClient("https://hsmkwrrptltywjqljrpf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96a3lrcm11aWtxZ3JqZndlbXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1NTgzOTQsImV4cCI6MjAyNDEzNDM5NH0.uw5P0WARTEleBzhNtraLsCEO2wp781rSzggxDI9dm8s");
+
 
 export default supabase;
