1a2482e5de22fa8c0545dd30834e9b199bd96195
Justin Kleiber <jkleiber8@gmail.com>
2025-01-10 16:59:38 -0600

    #78: adds support for pit vs. match scouting data upload

--- util/main.py
+++ util/main.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--infile", default="data.json")
     parser.add_argument("--outfile", default="data.csv")
-    parser.add_argument("--offline_mode", default="csv", choices=["csv", "upload"])
+    parser.add_argument("--offline_mode", default="upload", choices=["csv", "upload"])
+    parser.add_argument("--type", default="match", choices=["match", "pit"])
     args = parser.parse_args()
 
     # Parse the JSON array in the provided file into a CSV or upload directly to supabase based on mode.
-    process_offline_data_file(args.infile, args.outfile, args.offline_mode)
+    process_offline_data_file(args.infile, args.outfile, args.offline_mode, args.type)
