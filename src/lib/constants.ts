4c23245104cec1517587e3e1f3bc0a9c91d402ca
Justin Kleiber <jkleiber8@gmail.com>
2025-03-15 11:25:17 -0400

    Resets to use nearest event

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,4 +7,4 @@ export const teamInfoTable = "Team";
 // Event information.
 export const defaultEventId = "2025vapor";
 // Flag to force override to the default event. Useful if you are playing two tournaments on the same day.
-export const useDefaultEvent = true;
+export const useDefaultEvent = false;
