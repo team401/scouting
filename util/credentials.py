5b47ad5ab02ec1cba2afd74cbf48e3979793bf63
Josh <joshua.azro@gmail.com>
2025-10-08 19:30:22 -0400

    new supabase db

--- util/credentials.py
+++ util/credentials.py
@@ -2,8 +2,8 @@
 import json
 
 # Public supabase credentials.
-SUPABASE_URL = "https://hsmkwrrptltywjqljrpf.supabase.co"
-SUPABASE_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzbWt3cnJwdGx0eXdqcWxqcnBmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTcyODQ5MiwiZXhwIjoyMDQ1MzA0NDkyfQ.qgVkNvWkDXiaiS9GDzG-1fFYkf3KnheVpIJr4B3XfFk"
+SUPABASE_URL = "https://fmftrvccnugpwofcyqxf.supabase.co"
+SUPABASE_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZnRydmNjbnVncHdvZmN5cXhmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTg3NDc3OSwiZXhwIjoyMDc1NDUwNzc5fQ.xap4yX7yadruUlHil26rK8z42MoclQji8z3pUUijNik"
 
 def get_tba_credentials(input_creds, file="private_credentials.json"):
     tba_creds = None
