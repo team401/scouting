12d4d7450cb2591d51d06cef65d0771150a2f661
Josh <joshua.azro@gmail.com>
2025-10-10 16:35:53 -0400

    had an extra dot bru

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-export const projectId = "oltmplswnrxlhmordqki."
+export const projectId = "oltmplswnrxlhmordqki"
 export const supabase = createClient("https://" + projectId + ".supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sdG1wbHN3bnJ4bGhtb3JkcWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5NDg1ODAsImV4cCI6MjA3NTUyNDU4MH0.K78JxtZvx2QUS2B8YimMjYzA5f5olHag-3QVoM6-Or0");
