bc760638df2c425e91228b3da58c3d0cdc48f663
Josh <joshua.azro@gmail.com>
2025-10-10 16:33:20 -0400

    updating wrong part lmao

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-export const projectId = "hsmkwrrptltywjqljrpf"
-export const supabase = createClient("https://" + projectId + ".supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzbWt3cnJwdGx0eXdqcWxqcnBmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTcyODQ5MiwiZXhwIjoyMDQ1MzA0NDkyfQ.qgVkNvWkDXiaiS9GDzG-1fFYkf3KnheVpIJr4B3XfFk");
+export const projectId = "oltmplswnrxlhmordqki."
+export const supabase = createClient("https://" + projectId + ".supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sdG1wbHN3bnJ4bGhtb3JkcWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5NDg1ODAsImV4cCI6MjA3NTUyNDU4MH0.K78JxtZvx2QUS2B8YimMjYzA5f5olHag-3QVoM6-Or0");
